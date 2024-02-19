17e0e25d7b3f8908081939ea44be9badda576a79
JWord <jwren4170@att.net>
2024-02-19 16:41:25 -0600

    Seperate files for routes and router

--- public/index.php
+++ public/index.php
@@ -2,17 +2,6 @@
 
 require_once '../helpers.php';
 
-$routes = [
-    '/workopia/' => 'controllers/home.php',
-    '/workopia/listings' => 'controllers/listings/index.php',
-    '/workopia/listings/create' => 'controllers/listings/create.php',
-    '404' => 'controllers/error/404.php'
-];
-
 $uri = $_SERVER['REQUEST_URI'];
 
-if (array_key_exists($uri, $routes)) {
-    require_once base_path($routes[$uri]);
-} else {
-    require_once base_path($routes['404']);
-}
+require_once base_path('router.php');
---
+++ routes.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    '/workopia/' => 'controllers/home.php',
+    '/workopia/listings' => 'controllers/listings/index.php',
+    '/workopia/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/error/404.php'
+];
